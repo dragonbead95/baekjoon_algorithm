bd4039049f1b07cd552a54750541fd31171373b2
yonghwanKim <dragonbead95@naver.com>
2021-04-14 22:26:45 +0900

    solution boj 2751

--- backjoon/src/PROM_43105/Solution.java
+++ backjoon/src/PROM_43105/Solution.java
@@ -1,23 +1,24 @@
 package PROM_43105;
 
 public class Solution {
-	public static int[][] cache;
-	public static int down(int[][] triangle, int row, int col)
+	
+	public static int down(int[][] triangle, int row, int col, int[][] cache)
 	{
 		if(row>=triangle.length)
 		{
-			return triangle[row-1][col];
+			return cache[row-1][col] = triangle[row-1][col];
 		}
+		
 		if(row==0 && col==0)
 		{
-			return down(triangle, row+1, col);
+			return down(triangle, row+1, col, cache);
 		}
 		
 		if(cache[row][col]!=0)
 		{
 			return cache[row][col];
 		}
-		return cache[row-1][col] = triangle[row-1][col] + Math.max(down(triangle,row+1,col), down(triangle,row+1,col+1));
+		return cache[row-1][col] = triangle[row-1][col] + Math.max(down(triangle,row+1,col,cache), down(triangle,row+1,col+1,cache));
 		
 	}
 	
@@ -35,15 +36,15 @@ public class Solution {
 	
 	public static int solution(int[][] triangle)
 	{
-		cache = new int[triangle.length][triangle.length];
-		int answer = down(triangle, 0,0);
+		int[][] cache = new int[triangle.length][triangle.length];
+		int answer = down(triangle, 0,0, cache);
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int[][] triangle = {{8},{2,7},{4,5,2}};
-		int[][] triangle = {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
+		int[][] triangle = {{8},{2,7},{4,5,2}};
+//		int[][] triangle = {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
 		System.out.println(solution(triangle));
 	}
 }
